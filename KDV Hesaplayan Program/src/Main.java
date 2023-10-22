6ce5a9e3c5b30d8c4666707459dcd9393e7cd4e2
Gökhan Acar <gokhannacarr@hotmail.com>
2023-10-22 14:27:49 +0300

    kdvCalculator

---
+++ KDV Hesaplayan Program/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double kdv1 = 0.18, kdv2 = 0.08;
+
+        System.out.print("Please enter the amount: ");
+        double amount = input.nextInt();
+
+        if ((amount <= 1000) && (amount >= 0))
+        {
+            amount = amount + (amount * kdv1);
+        }
+        else if (amount > 1000)
+        {
+            amount = amount + (amount * kdv2);
+        }
+        System.out.println("Your Money With KDV:" +amount);
+    }
+}
